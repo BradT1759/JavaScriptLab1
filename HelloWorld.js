3707f2606d1ed729f501e0648772aad56bcc4c2e
BradT1759 <BradT1759@users.noreply.github.com>
2019-09-15 20:10:03 -0300

    Add files via upload
    Created this in notepad based on previous knowledge and experiences with JavaScript in my first couple of semesters.
    I hope it is acceptable.

---
+++ HelloWorld.js
@@ -0,0 +1,16 @@
+/**
+ * Date: Sunday, 4:41 PM EST
+ * @author BradT
+ */
+public class Week1aHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This program prints a simple message
+        System.out.println("Hello world!");
+        
+    }
+    
+}
